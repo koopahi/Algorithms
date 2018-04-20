961201ef085ed289c32c4ecada3cab760f610e10
koopahi <ek2833@nyu.edu>
2018-04-20 13:00:04 -0400

    Create matching_closing_parenthesis.py

---
+++ challenges/matchingClosingParenthesis/solutions/matching_closing_parenthesis.py
@@ -0,0 +1,20 @@
+def matching_closing_parenthesis(s):
+    l=[]
+    for index in range(len(s)):
+        if s[index]=='(':
+            l.append(index)
+        elif s[index]==')':
+            try:
+                print(l.pop(),',', index)
+            except IndexError:
+                print("Close parenthesis at", index, "does not match with any open paranthesis")
+        
+    while len(l):
+                print("open parenthesis at", l.pop(), "does not match with any close paranthesis")
+
+
+def test_matching():
+    matching_closing_parenthesis("asdasdad")
+    matching_closing_parenthesis("asdad(")
+    matching_closing_parenthesis("adsad(asda)(asdadeh0())")
+    matching_closing_parenthesis("")
